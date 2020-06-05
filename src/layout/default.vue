66c01d9394e68408b07194b80a69f013ca78a22f
yelt123 <1285485655@qq.com>
2020-06-05 13:21:39 +0800

    edition1

---
+++ src/layout/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="page-default">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style scoped>
+</style>
