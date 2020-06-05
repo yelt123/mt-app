240125373f54f1ab45eccefefc9a0ea4b2d04df1
yelt123 <1285485655@qq.com>
2020-06-05 13:38:28 +0800

    edition1

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import defaultPage from '@/layout/default'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld'
-    }
-  ]
+  routes: [{
+    path: '/',
+    name: 'defaultPage',
+    component: defaultPage
+  }]
 })
