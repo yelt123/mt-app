13e9d72e45bd383c842902edf306711f258e8cb7
yelt123 <1285485655@qq.com>
2020-06-05 18:03:45 +0800

    edition2

---
+++ src/components/header/searchBar.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="search-bar">search-bar</div>
+</template>
+<script>
+export default {}
+</script>
--- src/layout/default.vue
+++ src/layout/default.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="page-default">
+  <div class="layout-default">
     <el-container>
-      <el-header>Header</el-header>
+      <el-header>
+        <my-header />
+      </el-header>
       <el-main>Main</el-main>
       <el-footer>Footer</el-footer>
     </el-container>
   </div>
 </template>
 <script>
-export default {}
+import MyHeader from '@/components/header/index'
+export default {
+  components: { MyHeader }
+}
 </script>
-<style scoped>
+<style scoped lang='scss'>
+@import "@/assets/css/public/layout.scss";
 </style>
