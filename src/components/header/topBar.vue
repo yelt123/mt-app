d626b163d6594011db389f07d6394bbdb6c0ad20
yelt123 <1285485655@qq.com>
2020-06-06 09:52:40 +0800

    dev edition1

--- src/components/header/topBar.vue
+++ src/components/header/topBar.vue
@@ -1,8 +1,14 @@
 <template>
-<div class="top-bar">top-bar</div>
+  <div class="top-bar">
+    <el-row>
+      <el-col :span="10">left</el-col>
+      <el-col :span="14">right</el-col>
+    </el-row>
+  </div>
 </template>
 <script>
-export default {
-
-}
+export default {}
 </script>
+<style scoped lang='scss'>
+@import "@/assets/css/public/header/topbar.scss";
+</style>
